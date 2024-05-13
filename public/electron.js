fd914e678c45b3039025a27b8a6caa56d11a3e4b
jalowe13 <jplowe21@vt.edu>
2024-05-12 19:25:09 -0500

    Initial Electron Loading

---
+++ public/electron.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const url = require('url')
+
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // Start URL for production
+  const startUrl = process.env.ELECTRON_START_URL || url.format({
+    pathname: path.join(__dirname, '/../build/index.html'),
+    protocol: 'file:',
+    slashes: true
+  });
+
+  //load the index.html from a url
+  win.loadURL('http://localhost:3000');
+
+  win.webContents.openDevTools()
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})

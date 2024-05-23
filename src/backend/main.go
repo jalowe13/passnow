1451096553262da38325c7fbe9998590d1f75fe4
jalowe13 <jplowe21@vt.edu>
2024-05-22 23:47:34 -0500

    Create collection test in SQLite

--- src/backend/main.go
+++ src/backend/main.go
@@ -3,8 +3,14 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"time"
 	"net/http"
 	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/apis"
+	"github.com/pocketbase/pocketbase/models"
+    "github.com/pocketbase/pocketbase/models/schema"
+    "github.com/pocketbase/pocketbase/tools/types"
 	"github.com/pocketbase/pocketbase/core"
 )
 
@@ -27,23 +33,60 @@ func main() {
 	db := pocketbase.New()
 
 	fmt.Println("Pocketbase created...")
+	// serves static files from the provided public dir (if exists)
 	db.OnBeforeServe().Add(func(e *core.ServeEvent) error {
-		log.Println("Pocketbase is about to start...")
-		fmt.Println("Pocketbase is about to start...")
+		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
 		return nil
 	})
 	fmt.Println("Pocketbase before serve event added...")
 	// Start Pocketbase in a go routine to avoid blocking the main thread http listener
 	go func() {
-		err := db.Start()
-		if err != nil {
+		if err := db.Start(); err != nil {
 			log.Fatal(err)
-			fmt.Println("Pocketbase failed to start!")
-		} else {
-			fmt.Println("Pocketbase started successfully!")
 		}
 	}()
-	fmt.Println("Pocketbase started...")
+	time.Sleep(time.Second * 5) // Wait for 5 seconds to allow Pocketbase to start
+	fmt.Println("Pocketbase started!!!!!!!!!!!!!!!!!!!")
+
+	// Create a new collection, must go into a function
+	collection := &models.Collection{
+		Name:       "Rubys iaejwfaioj",
+		Type:       models.CollectionTypeBase,
+		ListRule:   nil,
+		ViewRule:   types.Pointer("@request.auth.id != ''"),
+		CreateRule: types.Pointer(""),
+		UpdateRule: types.Pointer("@request.auth.id != ''"),
+		DeleteRule: nil,
+		Schema:     schema.NewSchema(
+			&schema.SchemaField{
+				Name:     "name",
+				Type:     schema.FieldTypeText,
+				Required: true,
+				Options:  &schema.TextOptions{
+					Max: types.Pointer(10),
+				},
+			},
+			&schema.SchemaField{
+				Name:     "password",
+				Type:     schema.FieldTypeText,
+				Required: true,
+				Options:  &schema.TextOptions{
+					Max: types.Pointer(20), // Password max length
+				},
+			},
+		),
+		Indexes: types.JsonArray[string]{
+			"CREATE UNIQUE INDEX idx_name ON example (name)",
+		},
+	}
+	
+	if err := db.Dao().SaveCollection(collection); err == nil {
+		fmt.Println("Collection Not Saved"); 
+	}
+
+
+	log.Println("Record saved successfully!")
 	http.HandleFunc("/api/button-clicked", buttonClickedHandler)
 	http.ListenAndServe("localhost:8080", nil)
+
 }
